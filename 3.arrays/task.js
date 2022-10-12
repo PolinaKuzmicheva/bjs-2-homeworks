15b52b99f609578bdb76da65d8d1a4e4c12515ea
PolinaKuzmicheva <polinamalena@gmail.com>
2022-10-12 14:47:21 +0300

    arrays homework

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,11 @@
 function compareArrays(arr1, arr2) {
-  let result;
+  let result = arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
 
-  // Ваш код
-
-  return result; // boolean
+   return result;
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+  let resultArr =((arr.filter(number => number > 0)).filter(number => number % 3 === 0)).map(number => number * 10);
 
-  // Ваш код
-
-  return resultArr; // array
+  return resultArr; 
 }
