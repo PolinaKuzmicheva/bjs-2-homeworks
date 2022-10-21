a9bc46c07bc478259f773b6d58d6db2472f44dbc
PolinaKuzmicheva <polinamalena@gmail.com>
2022-10-21 22:26:51 +0300

    task objects added

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,45 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+
+
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+  if(this.marks === undefined){ 
+    this.marks = [mark];
+    } else {
+      this.marks.push(mark)
+    }
+}
+
+Student.prototype.addMarks = function(...rest) {
+  if(this.marks === undefined){ 
+    this.marks = [];
+    } else {
+      this.marks.push(...rest)
+    }
+}
+
+Student.prototype.getAverage = function() {
+  let sum = this.marks.reduce((acc, cv) => {
+    return acc + cv;
+}, 0);
+  return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
+
+
+// let student1 = new Student('Анна', 'женщина', 22);
+// let student2 = new Student('Саша', 'мужчина', 21);
+// let student3 = new Student('Иван', 'мужчина', 27);
